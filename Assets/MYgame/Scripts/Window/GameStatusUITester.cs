28c44714d7875daa9b2886081d2fac606ed5ac82
LanKuDot <airlanser@gmail.com>
2021-12-02 18:14:18 +0800

    Update the text in the tester

--- Assets/MYgame/Scripts/Window/GameStatusUITester.cs
+++ Assets/MYgame/Scripts/Window/GameStatusUITester.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using MYgame.Scripts.Scenes.GameScenes.Data;
 using UnityEngine;
 
@@ -17,14 +19,29 @@ namespace MYgame.Scripts.Window
         [SerializeField]
         private BrickAmount[] _brickAmounts;
 
+        private int _number;
+
         private void Start()
         {
             SetUI();
+            DOTween.To(
+                    () => 0, x => { }, 0, 0.5f)
+                .OnStepComplete(UpdateNumber)
+                .SetLoops(-1);
         }
 
         private void SetUI()
         {
             _statusUI.SetBuildingRecipe(_curImageSprite, _brickAmounts, _nextImageSprite);
+        }
+
+        private void UpdateNumber()
+        {
+            _number += 1;
+            _statusUI.UpdateTotalBricksNumber(StaticGlobalDel.EBrickColor.eRed, _number);
+            _statusUI.UpdateTotalBricksNumber(StaticGlobalDel.EBrickColor.eYellow, _number * 2);
+            _statusUI.UpdateTotalBricksNumber(StaticGlobalDel.EBrickColor.eGreen, _number * 3);
+            _statusUI.UpdateTotalBricksNumber(StaticGlobalDel.EBrickColor.eWhite, _number * 4);
         }
     }
 }
